c6e3ef149b374aa6002b9c5c30b00de4ea9b345b
isolano-veridas <isolano@veridas.com>
2023-10-23 20:50:05 +0200

    Add Lazy plugin manager

--- lua/isolano/packer.lua
+++ lua/isolano/packer.lua
@@ -1,9 +1,13 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
-
--- Only required if you have packer configured as `opt`
-vim.cmd.packadd('packer.nvim')
-
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-end)
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup(plugins, opts)
